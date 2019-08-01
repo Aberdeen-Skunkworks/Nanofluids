f3cd2f7775d4a7abe92c582bcd07e3959c971142
Skunkworks lab <m.bannerman@gmail.com>
2019-08-01 09:33:53 +0100

    Some updates to THW.py

--- VXI_Scripts/PythonController/THW.py
+++ VXI_Scripts/PythonController/THW.py
@@ -806,13 +806,14 @@ class THW:
             plt.pause(0.0001)
 
         plt.show()
+        
 #Our probe is this
 #https://uk.rs-online.com/web/p/platinum-resistance-temperature-sensors/2364299/?relevancy-data=636F3D3126696E3D4931384E525353746F636B4E756D626572266C753D656E266D6D3D6D61746368616C6C26706D3D5E2828282872737C5253295B205D3F293F285C647B337D5B5C2D5C735D3F5C647B332C347D5B705061415D3F29297C283235285C647B387D7C5C647B317D5C2D5C647B377D2929292426706F3D3126736E3D592673723D2673743D52535F53544F434B5F4E554D4245522677633D4E4F4E45267573743D32333634323939267374613D3233363432393926&searchHistory=%7B%22enabled%22%3Atrue%7D
 #1/5th DIN PT100 +-0.06C at 0C (in accordance with IEC 751)
 #This means the alpha is 0.00385 
 #Following the notes at http://educypedia.karadimov.info/library/c15_136.pdf
 #We can use the Callendar-Van Dusen expression to calculate T
-#
+
 # Here's a definition via the ITS-90 versus the IPTS-68
 #http://www.code10.info/index.php%3Foption%3Dcom_content%26view%3Darticle%26id%3D82:measuring-temperature-platinum-resistance-thermometers%26catid%3D60:temperature%26Itemid%3D83
 def RTD_RtoT(R, R0=100):
@@ -821,6 +822,21 @@ def RTD_RtoT(R, R0=100):
     #C = -4.183E-12
     Temp = (-R0 * A + math.sqrt(R0**2 * A**2 - 4 * R0 * B * (R0 - R)))  / (2 * R0 * B)
     return Temp
+
+def CvD(T):
+    '''The ratio of a platinum resistance to a resistance at 0 degc'''
+    A = 3.9083E-3
+    B = -5.775E-7
+    C = -4.183E-12
+    return 1+A*T + B*T**2- 100*C*T**3+ C*T**4
+
+#To predict a wire resistance we use the R=pL/A formula with p being the resistivity. At 20C p=1.06e-7.
+def wireR(L, T, D=0.000015):
+    p=1.06e-7 #conductivity at 20C
+    Awire=math.pi * (D/2)**2 #wire area
+    R20 = p * L / Awire
+    return R20 / CvD(20) * CvD(T)
+
 
 #Our wire lengths are 89.50mm and 60.50mm from pad end to pad end, and 0.015mm diameter
 #Short and Long resistances per length should be within 2% (pg 463 NIST THW)
